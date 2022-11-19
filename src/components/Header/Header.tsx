b5ef7df32b17cd52592457b4b548160dfa699664
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-11-19 17:11:00 +0100

    link to homepage on logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Header = ()=> {
+    
+    
     return (
         <div className="Header">
-            <h1 id="headerTitle">Koekboek!</h1>
+            <Link to="/"><h1 id="headerTitle">Koekboek!</h1></Link>
+            
             <p>Personal Random Recipehs</p>
         </div>
     )
