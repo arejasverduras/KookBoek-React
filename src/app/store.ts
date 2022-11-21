bedb02a2fc4977142d1a2feb78f2f2c71f5c7076
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-11-21 14:20:06 +0100

    remove boilerplate counter slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import recipehReducer from '../components/Recipehs/recipehSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     recipehs: recipehReducer
   },
 });
