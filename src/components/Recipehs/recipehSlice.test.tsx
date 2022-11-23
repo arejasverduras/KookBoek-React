432e6338018133536e8a04735c1f8f3eca6208f7
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-11-23 20:09:54 +0100

    write more unit tests for recipehSlice reducer to achieve 100% coverage

--- src/components/Recipehs/recipehSlice.test.tsx
+++ src/components/Recipehs/recipehSlice.test.tsx
@@ -4,8 +4,8 @@ import { renderWithProviders } from '../../utils-test';
 import { act } from 'react-dom/test-utils';
 import { userRecipehBook } from '../../database';
 
-import reducer, {setCurrentRecipeh, setFilter, setSearchTerm, initialState} from './recipehSlice';
-
+import reducer, {randomRecipeh, setCurrentRecipeh, setFilter, setSearchTerm, getSearchResults, initialState} from './recipehSlice';
+ 
 test('should return the initial state', ()=>{
        expect(reducer(undefined, {type: undefined})).toEqual(initialState) 
     }
@@ -62,5 +62,38 @@ test('should be able to change the searchTerm', ()=>{
     }
 
     expect(reducer(previousState, setSearchTerm("hoekie"))).toEqual(expectedState);
+ }
+)
+
+test('should be able to search the ingredients, voorkeur, categorie and instructions', ()=>{
+    const previousState = {
+        ...initialState, 
+        searchTerm: "paddenstoelen"
+    }
+    const expectedState = {
+        ...previousState,
+        searchResult: [userRecipehBook[0]]
+    }
+
+    expect(reducer(previousState, getSearchResults("paddenstoelen"))).toEqual(expectedState);
+ }
+)
+
+test('randomFunction should generate a not-seen recipeh at all times, or quit', ()=>{
+    const previousState = {
+        ...initialState, 
+        filteredRecipehs: [userRecipehBook[2], userRecipehBook[3], userRecipehBook[4]],
+        visitedRecipehs:  [4,2,1]
+    }
+    const expectedState = {
+        ...previousState,
+        searchResult: [userRecipehBook[0]]
+    }
+
+    const newState = reducer(previousState, randomRecipeh());
+
+    expect(newState.currentRecipeh).not.toEqual(userRecipehBook[1]);
+    expect(newState.currentRecipeh).not.toEqual(userRecipehBook[3]);
+    expect(newState.currentRecipeh).not.toEqual(userRecipehBook[0]);
  }
 )
