684dbb209bb885ff7e849af0eb249795bd603855
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-11-28 18:33:50 +0100

    should be able to show and hide the favorites

--- src/components/FavoritesList/FavoritesList.tsx
+++ src/components/FavoritesList/FavoritesList.tsx
@@ -1,30 +1,37 @@
-import React from "react";
+import React, {useState} from "react";
 import { useAppSelector } from "../../app/hooks";
 import { selectFavorites } from "../Recipehs/recipehSlice";
-import { RecipehTile } from "../Recipehs/VisitedList/RecipehTile/RecipehTile";
 import { FavoritesTile } from "./FavoritesTile/FavoritesTile";
-import { ToggleFavoriteButton } from "./toggleFavoriteButton/toggleFavortieButton";
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
 
 export const FavoritesList = () => {
     const favorites = useAppSelector(selectFavorites);
-
     const favoritesInTheList = favorites.length > 0? true : false;
+
+    const [showFavorites, toggleFavorites] = useState(true);
+
+    const toggleFavoritesHandler = () => {
+        toggleFavorites(showFavorites? false: true)
+    }
     
     if (favoritesInTheList){
-        // const removeFavoriteButtonClickHanlder = (id:number) => {
-        //     dispatch(removeFavorite(id));
-        // }
         const favoritesListItems = favorites.map((favoriteId, index) => 
-                // <RecipehTile title="favoritesListItem" id={favoriteId} showFavorite={true}/>
                 <FavoritesTile key={favoriteId} title="favoritesListItem" id={favoriteId} showFavorite={true}/>
         )
 
     return(
-        <div className="favoritesListComponent">
-            <h3>Favorites</h3>
+        <div className="favoritesListComponent" >
+            <div  onClick={toggleFavoritesHandler} >
+              <h3 className={showFavorites? "showHideFavoritesOpen": "showHideFavoritesClosed"}>
+                {showFavorites? 'Hide': 'My'} Favorites <FontAwesomeIcon 
+                    icon={faHeart} 
+                    className={showFavorites? "faHeartPlus":"faHeartMinus"}/>
+                </h3>
+            </div>
             <ul title="favoritesList" className="favoritesList">
-                {favoritesListItems}
+                {showFavorites? favoritesListItems:null}
             </ul>
         </div>  
     )} else {
