4a8d16ffe37e5728708d06c47e6dc591f939501e
Arejas <62893479+arejasverduras@users.noreply.github.com>
2022-12-16 11:55:53 +0100

    add icons to recipeh meta list

--- src/components/Recipehs/Recipeh/Recipeh.tsx
+++ src/components/Recipehs/Recipeh/Recipeh.tsx
@@ -1,11 +1,23 @@
 import React from "react";
 import { capitalize } from "../../../features/recipehFeatures";
+import { faUtensils, faLeaf, faSeedling, faCow, faFish, faClock, faPastafarianism, faBowlFood } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ToggleFavoriteButton } from "../../FavoritesList/toggleFavoriteButton/toggleFavortieButton";
 
 
 export const Recipeh =({current}:any) => {
     const ingredientsList = current.ingredienten.map((ingredient:string, index:number) => <li key={index}>{capitalize(ingredient)}</li>);
     const instructionList = current.instructie.map((instructie:string, index:number)=> <li key={index}>{instructie}</li>);
+
+    const iconMap = new Map();
+    iconMap.set('alles', faUtensils);
+    iconMap.set('vega', faLeaf);
+    iconMap.set('vegan', faSeedling);
+    iconMap.set('vlees', faCow);
+    iconMap.set('vis', faFish);
+    iconMap.set('kooktijd', faClock);
+    iconMap.set('categorie', faBowlFood)
+
 
     return (
         <div className="recipeh" title="recipeh" id="recipehTop">
@@ -16,12 +28,14 @@ export const Recipeh =({current}:any) => {
             </div>
             <h1 id="recipehTitle" title="realRecipehTitle">{current.naam}</h1>
 
-
+                {/* Extract to component?  */}
             <div className="recipehMetaList">
-                    <div >{current.voorkeur}</div>
-                    <div >{current.kooktijd}</div>
-                    <div>{current.categorie}</div>
+                    <div className="recipehMeta filter" ><FontAwesomeIcon icon={iconMap.get(current.voorkeur)} />{capitalize(current.voorkeur)}</div>
+                    <div className="recipehMeta cooktime"><FontAwesomeIcon icon={iconMap.get('kooktijd')} />{current.kooktijd} min.</div>
+                    <div className="recipehMeta category"><FontAwesomeIcon icon={iconMap.get('categorie')} />{capitalize(current.categorie)}</div>
             </div>
+
+
             <div className="recipehTextContainer">
                 <div className="recipehIngredientenSection">
                     <h2>Ingredienten</h2>
